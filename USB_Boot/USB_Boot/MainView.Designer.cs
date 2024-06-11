90aad1a8e8079485bd290d0df73eb9eb1c5c403e
LoPhong Corporation <lophongcorporation.teamos@gmail.com>
2024-06-11 09:56:02 +0700

    Source Code

---
+++ USB_Boot/USB_Boot/MainView.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace USB_Boot
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.simpleButton5 = new DevExpress.XtraEditors.SimpleButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 59);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(57, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Removable:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(106, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(517, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Select a removable...";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BASIC INFOMATION";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 102);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(41, 13);
+            this.labelControl2.TabIndex = 4;
+            this.labelControl2.Text = "File ISO:";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(629, 92);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 6;
+            this.simpleButton1.Text = "Browse";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "FORMAT INFOMATION";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(12, 181);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(83, 13);
+            this.labelControl3.TabIndex = 9;
+            this.labelControl3.Text = "Partition scheme:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(106, 173);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(598, 21);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.Text = "Select a type partition...";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(12, 235);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(73, 13);
+            this.labelControl4.TabIndex = 11;
+            this.labelControl4.Text = "Target system:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 227);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(598, 21);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(12, 294);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(82, 13);
+            this.labelControl5.TabIndex = 13;
+            this.labelControl5.Text = "Removable label:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 286);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(598, 21);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 23);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "STATUS";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox4.Font = new System.Drawing.Font("Cascadia Code", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(12, 377);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(692, 81);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.Text = "LoPhong Corporation - All with USB!\r\nVersion 1.0\r\nWebsite: lonamphong.com\r\nAuthor" +
+    ": LoPhong (Source Developer), User348 (Designer)\r\nStatus: Ready for format";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(537, 475);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 18;
+            this.simpleButton2.Text = "Start";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelControl6.Location = new System.Drawing.Point(0, 494);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(198, 13);
+            this.labelControl6.TabIndex = 19;
+            this.labelControl6.Text = "LoPhong Corporation - All rights reserved";
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(629, 475);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton3.TabIndex = 20;
+            this.simpleButton3.Text = "Close";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Location = new System.Drawing.Point(445, 475);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton4.TabIndex = 21;
+            this.simpleButton4.Text = "About";
+            this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(497, 94);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(126, 21);
+            this.textBox5.TabIndex = 22;
+            this.textBox5.Text = "Size";
+            // 
+            // simpleButton5
+            // 
+            this.simpleButton5.Location = new System.Drawing.Point(629, 49);
+            this.simpleButton5.Name = "simpleButton5";
+            this.simpleButton5.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton5.TabIndex = 23;
+            this.simpleButton5.Text = "Refresh";
+            this.simpleButton5.Click += new System.EventHandler(this.simpleButton5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(385, 21);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Select a file ISO you need...";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 507);
+            this.Controls.Add(this.simpleButton5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IconOptions.Image = global::USB_Boot.Properties.Resources.usb_flash_drive;
+            this.MaximizeBox = false;
+            this.Name = "MainView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoPhong Corporation - All with USB!";
+            this.Load += new System.EventHandler(this.MainView_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        protected System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        protected System.Windows.Forms.ComboBox comboBox2;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private System.Windows.Forms.TextBox textBox2;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private System.Windows.Forms.TextBox textBox5;
+        private DevExpress.XtraEditors.SimpleButton simpleButton5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ USB_Boot/USB_Boot/MainView.cs
@@ -0,0 +1,295 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace USB_Boot
+{
+    public partial class MainView : DevExpress.XtraEditors.XtraForm
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern SafeFileHandle CreateFileA(string lpFileName, uint dwDesiredAccess, uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        private SafeFileHandle _DriverHandle;
+        private System.IO.FileStream _DriverStream;
+
+        private const uint GENERIC_READ = 0x80000000;
+        private const uint GENERIC_WRITE = 0x40000000;
+        private const uint FILE_SHARE_READ = 0x00000001;
+        private const uint FILE_SHARE_WRITE = 0x00000002;
+        private const uint OPEN_EXISTING = 3;
+
+        System.IO.DriveInfo[] DriverInfo;
+
+        public MainView()
+        {
+
+            InitializeComponent();
+            
+            RefreshUSBList();
+            comboBox2.Items.Add("GPT (GUID Partition Table)");
+            comboBox2.Items.Add("MBR (Master Boot Record)");
+            comboBox2.Enabled = false;
+            comboBox2.SelectedIndexChanged += new EventHandler(comboBox2_SelectedIndexChanged);
+            textBox3.Enabled = false;
+            simpleButton1.Enabled = false;
+            simpleButton2.Enabled = false;
+           
+        }
+
+        void RefreshDrivers()
+        {
+            try
+            {
+                comboBox1.Items.Clear();
+
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+               
+                foreach (DriveInfo drive in drives)
+                {
+               
+                    if (drive.IsReady)
+                    {
+               
+                        if (drive.DriveType == DriveType.Removable)
+                        {
+                   
+                            comboBox1.Items.Add(string.Format("{0} [{1}]", drive.Name, drive.TotalSize / 1024 / 1024 / 1024 + " GB (Gigabyte)"));
+                        }
+                    }
+                }
+
+                _DriverHandle = CreateFileA("\\\\.\\H:", GENERIC_READ, FILE_SHARE_READ, IntPtr.Zero, OPEN_EXISTING, 0, IntPtr.Zero);
+                _DriverStream = new System.IO.FileStream(_DriverHandle, System.IO.FileAccess.Read);
+
+                byte[] bufferTest = new byte[512];
+                _DriverStream.Read(bufferTest, 0, 512);
+
+                _DriverStream.Close();
+                _DriverHandle.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            simpleButton1.Enabled = true;
+        }
+        private void RefreshUSBList()
+        {
+            comboBox1.Items.Clear();
+
+  
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+      
+            foreach (DriveInfo drive in drives)
+            {
+          
+                if (drive.IsReady)
+                {
+                 
+                    if (drive.DriveType == DriveType.Removable)
+                    {
+                      
+                        comboBox1.Items.Add(string.Format("{0} [{1}]" , drive.Name, drive.TotalSize / 1024 / 1024 / 1024 + " GB (Gigabyte)"));
+                    }
+                }
+            }
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+         
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "ISO Files (*.iso)|*.iso";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+       
+                textBox1.Text = dialog.FileName;
+             
+                string fileName = Path.GetFileNameWithoutExtension(dialog.FileName);
+
+       
+                textBox3.Text = fileName;
+                string isoPath = dialog.FileName;
+
+                long isoSize = new FileInfo(isoPath).Length;
+
+           
+                textBox5.Text = isoSize.ToString() + " bytes";
+
+              
+            }
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox3.Enabled = true;
+            
+
+            if (comboBox2.SelectedIndex == 0)
+            {
+       
+                textBox2.Text = "UEFI (Unified Extensible Firmware Interface)";
+            }
+            if (comboBox2.SelectedIndex == 1)
+            {
+                textBox2.Text = "BIOS (Basic Input Output System)";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            comboBox2.Enabled = true;
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainView_Load(object sender, EventArgs e)
+        {
+           RefreshDrivers();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            simpleButton2.Enabled = true;
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            Close();
+           
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you really want format USB? The data will be lost.", "LoPhong Corporation", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (result == DialogResult.OK)
+            {
+                DialogResult result1 = MessageBox.Show("Do you sure your choice? This is last warning!", "LoPhong Corporation", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (result1 == DialogResult.OK)
+                {
+                
+                    simpleButton4.Enabled = false;
+                    comboBox1.Enabled = false;
+                    simpleButton1.Enabled = false;
+                    comboBox2.Enabled = false;
+                    textBox3.Enabled = false;
+                    simpleButton2.Enabled = false;
+                    simpleButton3.Enabled = false;
+                  
+                    if (comboBox1.SelectedIndex == -1)
+                    {
+                        MessageBox.Show("Please choose a USB.", "LoPhong Corporation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    
+                    string usbName = comboBox1.SelectedItem.ToString();
+                    string driveLetter = usbName.Substring(0, 2);
+
+                    if (comboBox2.SelectedIndex == 1)
+                    {
+                        try
+                        {
+                            Process process = new Process();
+                            process.StartInfo.FileName = "cmd.exe";
+                            process.StartInfo.Arguments = "/c format " + driveLetter + " /FS:NTFS /Q";
+                            process.StartInfo.CreateNoWindow = true;
+                            process.Start();
+                            process.WaitForExit();
+
+
+                            Thread.Sleep(5000);
+                            string newUsbName = textBox3.Text;
+
+                            Process process1 = new Process();
+                            process1.StartInfo.FileName = "cmd.exe";
+                            process1.StartInfo.Arguments = "/c label " + driveLetter + newUsbName;
+                            process1.StartInfo.CreateNoWindow = true;
+                            process1.Start();
+                            process1.WaitForExit();
+
+
+                            Thread.Sleep(5000);
+
+
+                            // Get the selected ISO file path
+                            string isoFilePath = textBox1.Text;
+
+                            // Check if the ISO file exists
+                            if (!File.Exists(isoFilePath))
+                            {
+                                MessageBox.Show("The ISO file is not exist. Please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+
+                            // Check if a USB drive is selected
+                            if (comboBox1.SelectedIndex == -1)
+                            {
+                                MessageBox.Show("Please select a removable before process.");
+                                return;
+                            }          
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Failed to format USB. Please check and try again. ERROR MESSAGE:" + ex.Message, "LoPhong Corporation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            simpleButton4.Enabled = true;
+                            comboBox1.Enabled = true;
+                            simpleButton1.Enabled = true;
+                            comboBox2.Enabled = true;
+                            textBox3.Enabled = true;
+                            simpleButton2.Enabled = true;
+                            simpleButton3.Enabled = true;
+                        }
+                    }
+                }
+                else if (result1 == DialogResult.Cancel)
+                {
+
+                }
+            }
+            else if (result == DialogResult.Cancel)
+            {
+
+            }    
+        }
+
+        private void simpleButton5_Click(object sender, EventArgs e)
+        {
+            RefreshDrivers();
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+           DialogResult result = MessageBox.Show("Please visit my Github. The About form is unsupported. Visit here: github.com/LoPhong-Corporation-Official", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(result == DialogResult.Yes)
+            {
+                Process.Start("https://github.com/LoPhong-Corporation-Official");
+            }
+
+        }
+       
+    }
+}
